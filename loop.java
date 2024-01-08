36831698b2310f5ad9f2dc60b03aeab2d6892ae5
sakshibharath <151045201+sakshibharath@users.noreply.github.com>
2024-01-08 18:17:45 +0530

    Update loop.java

--- loop.java
+++ loop.java
@@ -2,8 +2,8 @@ public class loops {
 
 	public static void main(String[] args) {
 		int num,i;
-	        i=10;
-		num=sc.nextInt();
+	        i=0;
+		num=10;
 		for(i=0;i<=num;i++)
 		{
 			System.out.println(i);
