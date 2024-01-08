fa7ba1a1725c6c3d413cdfc8bee2954b55e37de2
sakshibharath <151045201+sakshibharath@users.noreply.github.com>
2024-01-08 18:14:00 +0530

    Add files via upload

---
+++ loop.java
@@ -0,0 +1,15 @@
+package basiccommands;
+public class loops {
+
+	public static void main(String[] args) {
+		int num,i;
+	        i=10
+		num=sc.nextInt();
+		for(i=0;i<=num;i++)
+		{
+			System.out.println(i);
+		}
+
+	}
+
+}
